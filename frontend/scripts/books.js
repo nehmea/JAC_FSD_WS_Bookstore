01d620992312e6de0de12dd21a5afbfa5ba94795
nehmea <ali.hassan.nehme@gmail.com>
2022-06-05 05:49:25 -0400

    first json to table for getAllBooks using JS

--- frontend/scripts/books.js
+++ frontend/scripts/books.js
@@ -7,7 +7,58 @@ function getAllBooks() {
             url: `${HOST}`
         }
     ).done((response) => {
+
+        $('#table').bootstrapTable({
+            pagination: true,
+            search: true,
+            columns: [{
+                field: 'id',
+                title: 'Item ID',
+              }, {
+                field: 'isbn13',
+                title: 'ISBN 13'
+              }, {
+                field: 'isbn10',
+                title: 'ISBN 10'
+              }, {
+                field: 'title',
+                title: 'Title'
+              }, {
+                field: 'language',
+                title: 'Language'
+              }, {
+                field: 'binding',
+                title: 'Format'
+              }, {
+                field: 'release_date',
+                title: 'Publication Date'
+              }, {
+                field: 'pages',
+                title: 'Pages'
+              }, {
+                field: 'dimensions',
+                title: 'Dimensions'
+              }, {
+                field: 'rating',
+                title: 'Rating'
+              }, {
+                field: 'publisher',
+                title: 'Publisher'
+              }, {
+                field: 'authors',
+                title: 'Author(s)'
+              }, {
+                field: 'numberOfCopies',
+                title: 'Number of copies'
+              }, {
+                field: 'edition',
+                title: 'Edition'
+              }],
+              data: response
+            
+        });
        document.getElementById('targetDiv').append(JSON.stringify(response, undefined, 2));
+       $("#targetDiv")[0].scrollIntoView();
     }).fail((obj, textStatus) => {
         if(obj && obj.responseJSON && obj.responseJSON.message) {
             alert(obj.responseJSON.message);
